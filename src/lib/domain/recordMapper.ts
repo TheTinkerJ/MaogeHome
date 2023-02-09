ee95bdab1f4e8f3f397368d2ca731ae9f8ac1b5a
maoge <1228672201@qq.com>
2023-02-09 21:16:56 +0800

    opt(账本): 调整流水排序方式

--- src/lib/domain/recordMapper.ts
+++ src/lib/domain/recordMapper.ts
@@ -21,7 +21,7 @@ export async function queryTradeFlowPage(params:ITradeFlowRecordPageRequest) {
     let start = params.page*params.pageSize;
     let offset = params.pageSize;
 
-    let querySql = `SELECT * FROM trade_flow_record limit ${start},${offset}`;
+    let querySql = `SELECT * FROM trade_flow_record limit ${start},${offset} order by modified_time desc`;
         
     let mysqlconn = await getMysqlConnFn()
     let records:ITradeFlowRecord[] = await mysqlconn
